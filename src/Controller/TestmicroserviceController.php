eb1a234edeb3640c14bfe892fdc442ef51542841
[math-borges] <[m.borges@brun-invest.net]>
2023-06-07 10:55:28 +0200

    Edit to see customer and customersList

--- src/Controller/TestmicroserviceController.php
+++ src/Controller/TestmicroserviceController.php
@@ -2,16 +2,25 @@
 namespace App\Controller;
 
 use App\Entity\Customer;
+use App\Repository\CustomerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestmicroserviceController extends AbstractController
 {
-    #[Route('/testmicroservice/{id}', name: 'app_testmicroservice')]
-    public function index(Customer $customer): JsonResponse
+    
+    #[Route('/testmicroservice/customers', name: 'app_testmicroservice_customers')]
+    public function getGustomers(CustomerRepository $customerRepository): JsonResponse
+    {
+        $customers = $customerRepository->findAll();
+        return $this->json($customers, 200, []);
+        
+    }
+    #[Route('/testmicroservice/customers/customer/{id}', name: 'app_testmicroservice_customers_customer')]
+    public function getCustomer(Customer $customer): JsonResponse
     {
         return $this->json($customer, 200, []);
         
-    }
+    }    
 }
